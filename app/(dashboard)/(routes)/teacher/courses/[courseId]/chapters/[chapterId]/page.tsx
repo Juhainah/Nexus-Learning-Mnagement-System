5717083362797ac34012f97cfd882e3020797c9a
Juhainah <juhainah333@gmail.com>
2024-03-10 11:46:49 +0530

    spelling chapter

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -61,7 +61,7 @@ const ChapterIdPage= async ({
         {!chapter.isPublished  && (
             <Banner 
             variant='warning'
-            label="This chapter is unbublished. It will not be visible in the course"
+            label="This chapter is unpublished. It will not be visible in the course"
             />
         )}
         <div className="p-6">
