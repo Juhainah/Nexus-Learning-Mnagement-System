89e3f0e4a47109b89a4b4ec1d9ab1c9cc9015fa2
Juhainah <95189314+Juhainah@users.noreply.github.com>
2024-03-10 12:27:15 +0530

    Update teacher.ts

--- lib/teacher.ts
+++ lib/teacher.ts
@@ -1,3 +1,9 @@
 export const isTeacher = (userId?: string | null) => {
-  return userId === process.env.NEXT_PUBLIC_TEACHER_ID;
+  if (!userId || !process.env.NEXT_PUBLIC_TEACHER_ID) {
+    return false; // Return false if userId or NEXT_PUBLIC_TEACHER_ID is not provided
+  }
+  
+  const teacherIds = process.env.NEXT_PUBLIC_TEACHER_ID.split(',');
+
+  return teacherIds.includes(userId);
 }
